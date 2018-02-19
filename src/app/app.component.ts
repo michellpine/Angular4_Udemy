4f6bf4678a35efa2abd8459a2ef4960cf36f503f
Michell <michellpine@hotmail.com>
2018-02-19 14:28:24 -0500

    passin data value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,8 @@ export class AppComponent {
     isFavorite: true
   };
 
-  onFavoriteChanged() {
-    console.log("Favorite changed");
+  onFavoriteChanged(isFavorite) {
+    console.log("Favorite changed: ", isFavorite);
   }
 }
 
