36080be89c8b1cbead0caefd06c0b7d706097063
Michell <michellpine@hotmail.com>
2018-02-19 10:06:53 -0500

    two way binding

--- src/app/provingComponent/courses.component.ts
+++ src/app/provingComponent/courses.component.ts
@@ -2,26 +2,17 @@ import { Component } from '@angular/core';
 import { CoursesService } from '../course/courses.service';
 
 @Component({
-    // <courses> 'courses'
-    // <div class="courses"> '.courses'
-    // <div id="courses"> '#courses'
     selector: 'app-courses',
     template: `
-            <!-- 
-            old way
-            <input (keyup.enter)="onKepUp($event)" />
-            -->
-            <input #email (keyup.enter)="onKepUp(email.value)" />
+            <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
             `
 })
 
 export class CoursesComponent {
-    // old way
-    // onKepUp($event) {
-    //    console.log($event.target.value);
-    // }
-    onKepUp(email) {
-            console.log(email);
+    email = "me@exmaple.com";
+
+    onKeyUp() {
+        console.log(this.email);
     }
 }
 
