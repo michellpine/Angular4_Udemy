2157a7a039eaf42dec322292075c60a62b014d14
Michell <michellpine@hotmail.com>
2018-02-19 10:48:30 -0500

    default pipes

--- src/app/provingComponent/courses.component.ts
+++ src/app/provingComponent/courses.component.ts
@@ -4,15 +4,21 @@ import { CoursesService } from '../course/courses.service';
 @Component({
     selector: 'app-courses',
     template: `
-            <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
-            `
+    {{ course.title | uppercase | lowercase }} <br />
+    {{ course.students | number }} <br />
+    {{ course.rating | number:'2.1-1' }}<br />
+    {{ course.price | currency: 'AUD':true:'3.2-2' }}<br />
+    {{ course.releaseDate | date: 'shortDate' }}
+    `
 })
 
 export class CoursesComponent {
-    email = "me@exmaple.com";
-
-    onKeyUp() {
-        console.log(this.email);
-    }
+    course = {
+        title: "Angular Complete",
+        rating: 4.9745,
+        students: 30123,
+        price: 190.95,
+        releaseDate: new Date(2016, 3, 1)
+    };
 }
 
