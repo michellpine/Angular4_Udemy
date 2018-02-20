5b86114600d40515ac2034e61e9b792dd33c2403
Michell <michellpine@hotmail.com>
2018-02-20 10:04:54 -0500

    hidden property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,6 @@ import { FavoriteChangedEventArgs } from './favorite/favorite.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses = [1, 2 ];
+  courses = [];
 }
 
