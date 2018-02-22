fb9e227ef47aa8e91568ac794bb2677adb44526c
Michell <michellpine@hotmail.com>
2018-02-22 14:55:11 -0500

    creating data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { FavoriteChangedEventArgs } from './favorite/favorite.component';
-
 
 @Component({
   selector: 'app-root',
@@ -8,50 +6,6 @@ import { FavoriteChangedEventArgs } from './favorite/favorite.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  tweet = {
-    body: 'Here is the body of a tweet...',
-    isLiked: false,
-    likesCount: 0
-  };
-  courses = [
-    { id: 1, name: 'course1'},
-    { id: 2, name: 'course2'},
-    { id: 3, name: 'course3'},
-  ];
-  subjects;
-  viewMode = 'a';
-  canSave = true;
 
-  task = {
-    title: 'Review applications',
-    assignee: null
-    /*{
-      name: 'John Smith'
-    }*/
-  };
-  onAdd() {
-    this.courses.push({ id: 4, name: 'course4' });
-  }
-
-  onRemove(course) {
-    let index = this.courses.indexOf(course);
-    this.courses.splice(index, 1);
-  }
-
-  onChange(course) {
-    course.name = 'Updated';
-  }
-
-  loadSubjects() {
-    this.subjects = [
-      { id: 1, name: 'subject1' },
-      { id: 2, name: 'subject2' },
-      { id: 3, name: 'subject3' }
-    ];
-  }
-
-  trackCourse(index, subject) {
-    return subject ? subject.id : undefined;
-  }
 }
 
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -7,15 +7,25 @@ import { Http } from '@angular/http';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent  {
-  tickets: any[];
-  API_ENDPOINT = 'http://localhost:8282/api/vehicles/';
-  //API = 'http://jsonplaceholder.typicode.com/posts';
+  posts: any[];
+  private url = 'http://jsonplaceholder.typicode.com/posts';
 
-  constructor(http: Http) {
-    http.get(this.API_ENDPOINT)
+  constructor(private http: Http) {
+    http.get(this.url)
     .subscribe(response => {
       console.log(response.json());
-      this.tickets = response.json();
+      this.posts = response.json();
     });
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post = { title: input.value };
+    input.value = ' '
+      this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        post['id'] = response.json().id;
+        this.posts.splice(0, 0, post);
+        console.log(response.json);
+      })
   }
 }
